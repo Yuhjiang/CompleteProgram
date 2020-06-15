042d26b106383597dd015dc3f8766f6c4d6b60de
jiangyuhao <jiang.yuhao0809@gmail.com>
2020-06-15 22:04:32 +0800

    更新桥接模式和组合模式

--- Design_Pattern/结构型模式/Adapter.py
+++ Design_Pattern/结构型模式/Adapter.py
@@ -13,7 +13,7 @@ class Adapter(Target, Adaptee):
         return f'Adapter: (TRANSLATED) {self.specific_request()[::-1]}'
 
 
-def client_code(target: 'Target') -> None:
+def client_code(target: Target) -> None:
     print(target.request(), end='')
 
 
